b6b9d2a800b6e0dab9c51d69ee6d9fdbbdfcc552
FaizGG_ <85857260+faizzzlol@users.noreply.github.com>
2023-11-20 19:58:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 // Initialize the map
 var mymap = L.map('map').setView([0, 0], 1);
 
-// Add a tile layer (you can replace this with other tile providers)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
+// Replace this with the URL of your own map tiles
+var mapTilesUrl = 'https://your-minecraft-map-tiles/{z}/{x}/{y}.png';
+
+// Add your own tile layer
+L.tileLayer(mapTilesUrl, {
+    attribution: 'Your attribution here'
 }).addTo(mymap);
 
-// Add markers for Minecraft-like points of interest
+// You can add markers or other features as needed
 var poi1 = L.marker([10, 10]).addTo(mymap);
 var poi2 = L.marker([-10, -10]).addTo(mymap);
 
-// Add popups to the markers
 poi1.bindPopup("<b>Minecraft Village</b><br>Coordinates: (10, 10)").openPopup();
 poi2.bindPopup("<b>Dungeon</b><br>Coordinates: (-10, -10)").openPopup();
