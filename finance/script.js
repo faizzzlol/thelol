f705bc3c61d5c4ff13d7696fea342848c244fd2d
FaizGG_ <85857260+faizzzlol@users.noreply.github.com>
2023-12-10 16:51:53 +0800

    Update script.js

--- finance/script.js
+++ finance/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Simulate fetching exchange rate from an API
     setTimeout(() => {
-        document.getElementById('exchange-rate').innerText = '1 USD = 4.17 MYR';
+        document.getElementById('exchange-rate').innerText = '1 Diamond = 0.05 TL$';
     }, 2000);
 
     // Simulate loading a price chart using Highcharts
